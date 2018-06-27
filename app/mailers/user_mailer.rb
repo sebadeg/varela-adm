a02c28a51932e85b078bf78c39981612ad57853a
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-06-27 17:54:43 -0300

    Novedades

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,4 @@
 class UserMailer < ApplicationMailer
-
 	def aceptar_usuario(usuario)
 		@usuario = usuario
 		#mail(to: usuario.email, bcc:'soporte@varela.edu.uy', subject: 'Envío de contraseña de acceso')
@@ -17,5 +16,4 @@ class UserMailer < ApplicationMailer
 
 		mail(to: 'soporte@varela.edu.uy', subject: 'Novedades', delivery_method_options: delivery_options)
 	end
-
 end
