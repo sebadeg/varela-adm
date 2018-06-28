178b599a37ac28bacae308390052381beb98006b
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-06-28 12:42:40 -0300

    Nuevo

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,7 +1,7 @@
 module EmailHelper
     def email_image_tag(image, **options)
 		attachments.inline[image] = {
-                                :data => File.read("#{Rails.root.to_s}/app/assets/images/#{image}"),
+                                :data => File.read(Rails.root.join("data","#{image}")),
                                 :mime_type => "image/jpg",
                                 :encoding => "base64"
                               }
