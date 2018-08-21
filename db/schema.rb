b807d9192a380cb2bb6adc1d82c6ef351c9bbb8a
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-08-21 09:20:02 -0300

    Inscripcion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180820122639) do
+ActiveRecord::Schema.define(version: 20180821010255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -268,12 +268,13 @@ ActiveRecord::Schema.define(version: 20180820122639) do
     t.string   "email2"
     t.string   "celular2"
     t.boolean  "registrado"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "grado"
     t.integer  "matricula"
     t.boolean  "visa"
     t.integer  "cedula"
+    t.boolean  "inhabilitado"
     t.index ["alumno_id"], name: "index_inscripcion_alumnos_on_alumno_id", using: :btree
     t.index ["convenio_id"], name: "index_inscripcion_alumnos_on_convenio_id", using: :btree
     t.index ["grado_id"], name: "index_inscripcion_alumnos_on_grado_id", using: :btree
