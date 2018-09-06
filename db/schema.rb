c48d19ed15743aa1f672c8c1fed835caf5bbb24f
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-09-06 12:46:48 -0300

    Inscripcion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904012900) do
+ActiveRecord::Schema.define(version: 20180904012539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
