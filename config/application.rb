aeffacd9a853527d42329b762781a1346e6b8a08
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-09-06 11:42:47 -0300

    Inscripciones

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module VarelaAdm
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 	config.i18n.default_locale = :es
+	config.assets.initialize_on_precompile = false
   end
 end
