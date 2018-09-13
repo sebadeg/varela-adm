20acbb7ec13e141a5603aef4649a16c127b4a68e
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-09-12 22:46:37 -0300

    Informe

--- app/admin/informe_inscripcion.rb
+++ app/admin/informe_inscripcion.rb
@@ -3,14 +3,30 @@ ActiveAdmin.register_page "Informe" do
   menu priority: 1, label: "Informe", parent: "Inscripción"
 
   content do
-    form decorate: true do |f|
-      table_for ActiveRecord::Base.connection.execute("SELECT *,proximo_grados.id as grado1 FROM inscripcion_alumnos INNER JOIN proximo_grados ON inscripcion_alumnos.grado=proximo_grados.id WHERE registrado ORDER BY proximo_grados.nombre"), sortable: true, class: 'index_table' do
-        column "Alumno" do |x| x["alumno_id"] end
-        column "Nombre" do |x| (alumno = Alumno.find(x["alumno_id"])) != nil ? "#{alumno.nombre} #{alumno.apellido}" : "" end
-        column "Grado" do |x| ProximoGrado.find(x["grado1"]).nombre end
-        column "Inscripto" do |x| x["inscripto"] end
-      end
+    collection = ActiveRecord::Base.connection.execute(
+        "SELECT * FROM
+        ((SELECT alumnos.id as alumno1, alumnos.nombre || ' ' || alumnos.apellido as alumnonombre1, proximo_grados.id as grado1 , proximo_grados.nombre as gradonombre1, inscripcion_alumnos.inscripto AS inscripto1
+        FROM alumnos INNER JOIN inscripcion_alumnos ON alumnos.id=inscripcion_alumnos.alumno_id INNER JOIN proximo_grados ON inscripcion_alumnos.grado=proximo_grados.id
+        WHERE registrado)
+        UNION
+        (SELECT null as alumno1, inscripciones.nombre as alumnonombre1, proximo_grado_id as grado1 , proximo_grados.nombre as gradonombre1, true AS inscripto1
+        FROM inscripciones INNER JOIN proximo_grados ON inscripciones.proximo_grado_id=proximo_grados.id)) AS foo
+        ORDER BY foo.gradonombre1" 
+         #LIMIT #{30} OFFSET #{30*(params[:page] == nil ? 0 : params[:page])}"
+         )
+
+    table_for collection, sortable: true, class: 'index_table' do
+      column "Alumno" do |x| x["alumno1"] end
+      column "Nombre" do |x| x["alumnonombre1"] end
+      column "Grado" do |x| x["gradonombre1"] end
+      column "Inscripto" do |x| x["inscripto1"] end
     end
+
+    div do 
+      collection.count
+    end
+
+
   end
 
   sidebar "Filtros" do
@@ -18,4 +34,5 @@ ActiveAdmin.register_page "Informe" do
       render "search_user"
     end
   end
+
 end
