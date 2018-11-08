09f223dd9abbe190fa0238dedd713e10211a1453
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-11-07 22:51:38 -0300

    Inicial

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.middleware.use Rails::Rack::LogTailer
 end
