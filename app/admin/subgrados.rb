80939b193cbb594f0010eb092df125a68bd7a384
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-11-14 12:43:12 -0300

    Seguimiento

--- app/admin/subgrados.rb
+++ app/admin/subgrados.rb
@@ -35,14 +35,8 @@ ActiveAdmin.register Subgrado do
           t.column :nombre
           t.column :apellido
 
-          t.column "Registrado" do |x| 
-            insc = InscripcionAlumno.where( "alumno_id=#{x.id}" ).first rescue nil
-            if insc != nil insc.registrado end
-          end
-          t.column "Inscripto" do |x|
-            insc = InscripcionAlumno.where( "alumno_id=#{x.id}" ).first rescue nil
-            if insc != nil insc.inscripto end
-          end
+          t.column "Registrado" do |x| (InscripcionAlumno.where( "alumno_id=#{x.id}" ).first rescue nil) != nil ? InscripcionAlumno.where( "alumno_id=#{x.id}" ).first.registrado : "" end
+          t.column "Inscripto" do |x| (InscripcionAlumno.where( "alumno_id=#{x.id}" ).first rescue nil) != nil ? InscripcionAlumno.where( "alumno_id=#{x.id}" ).first.inscripto : "" end
 
         end
       end
