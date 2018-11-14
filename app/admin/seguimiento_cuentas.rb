b69cd424e4ebdf67d2dcad08611c52ecfc7dade9
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-11-14 15:34:59 -0300

    Seguimiento

--- app/admin/seguimiento_cuentas.rb
+++ app/admin/seguimiento_cuentas.rb
@@ -8,12 +8,24 @@ ActiveAdmin.register_page "Seguimiento Cuenta" do
       column do
         panel "Cuentas" do
           ul do
-            Cuenta.all do |c|
-              li link_to "Habilitar", admin_cuenta_path(c), method: :get 
+            Cuenta.all.each do |c|
+              li link_to "Seguimiento", admin_cuenta_path(c), method: :get 
             end
           end
         end
       end
+
+      column do
+        panel "Cuentas" do
+          table_for Cuenta.all.each do |c|
+            :id
+            actions defaults: false do |u|
+              item "Seguimiento", admin_cuenta_path(u), title: "Seguimiento"
+            end
+          end
+        end
+      end
+
     end
   end
 
