45a5bf6ccae0fb7f25e00edb41a1b21e7798046f
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-11-14 16:46:56 -0300

    Seguimiento

--- app/admin/seguimiento_cuentas.rb
+++ app/admin/seguimiento_cuentas.rb
@@ -7,8 +7,10 @@ ActiveAdmin.register_page "Seguimiento Cuenta" do
     columns do
       column do
         panel "Cuentas" do
-          table_for Cuenta.where("id=#{params[:cuenta]}").each do |c|
+          table_for Alumno.where("id IN (SELECT alumno_id FROM cuenta_alumnos WHERE cuenta_id=#{params[:cuenta]})").each do |c|
             column :id
+            column :nombre
+            column :apellido
             column "Seguimiento" do |x|
               link_to 'Seguimiento', admin_cuenta_path(x), method: :get
             end
