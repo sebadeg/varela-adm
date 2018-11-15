ec11c19a5e9b815a4fa32fe799cedc8b888d709c
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-11-15 11:54:27 -0300

    Seguimiento

--- app/admin/seguimientos.rb
+++ app/admin/seguimientos.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Seguimiento do
 
   permit_params :id, :alumno_id, :celular, :no_atiende, :no_inscribe, :inscribe, :duda, :comentario, :created_at, :updated_at
 
-  action_item :localizar, only: :show do
+  action_item :localizar, only: :index do
     link_to 'Agregar', new_admin_seguimiento(:alumno_id => @alumno_id)
   end
 
@@ -44,9 +44,9 @@ ActiveAdmin.register Seguimiento do
 
   controller do
 
-    #def action_methods
-    #  super - ['new']
-    #end
+    def action_methods
+      super - ['new']
+    end
 
     def index
       @alumno_id = params["q"]["alumno_id_equals"]
