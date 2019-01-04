7a1b71efd48753a6d100f473e55ed2229e1e323e
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-01-04 18:23:41 -0300

    cuenta

--- app/admin2/pendiente.rb
+++ app/admin2/pendiente.rb
@@ -8,7 +8,6 @@ ActiveAdmin.register_page "Pendiente" do
     usuarios = Usuario.where( "mail IS NULL OR mail=false") rescue nil
     if ( usuarios != nil )
       usuarios.each do |usuario|
-        usuario = usuarios.first
         #usuario.update( password: usuario.passwd, password_confirmation: usuario.passwd );
         UserMailer.inscribir_usuario( usuario ).deliver_now
         ActiveRecord::Base.connection.execute( "UPDATE usuarios SET mail=true WHERE id=#{usuario.id};" )
