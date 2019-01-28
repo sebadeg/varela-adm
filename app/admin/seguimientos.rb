445c2b9a192f1b88fdba7aa9f9e1cd86427ecf5f
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-01-28 00:45:49 -0300

    Lista

--- app/admin/seguimientos.rb
+++ app/admin/seguimientos.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Seguimiento do
 
   menu priority: 1012, label: 'Seguimiento'
-  menu parent: 'Seguimiento'
+  menu priority: 1010, parent: 'Seguimiento'
 
   permit_params :id, :alumno_id, :celular, :no_atiende, :no_inscribe, :inscribe, :duda, :comentario, :created_at, :updated_at
 
