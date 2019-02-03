d4c92c38fcc11291911163a0bf9ca6f2942ee017
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-03 17:40:03 -0300

    Recargos

--- app/admin/pases.rb
+++ app/admin/pases.rb
@@ -8,24 +8,39 @@ ActiveAdmin.register InscripcionAlumno, as: 'Pase' do
   index do
   	#selectable_column
     column :alumno_id
+    column "Nombre" do |c| 
+      if c.alumno != nil
+        c.alumno.nombre
+      end
+    end
+    column "Apellido" do |c| 
+      if c.alumno != nil
+        c.alumno.apellido
+      end
+    end
     column :fecha_pase
     column :destino
     actions
   end
 
-  filter :alumno_id
+  filter :alumno_id, :label => 'Alumno', :as => :select, :collection => Alumno.all.order(:nombre,:apellido).map{|u| ["#{u.id} - #{u.nombre} #{u.apellido}", u.id]}
 
   show do
     attributes_table do
-      row :alumno_id
+      row "Alumno" do |r| (r.alumno != nil ? "#{r.alumno.id} - #{r.alumno.nombre} #{r.alumno.apellido}" : "") end
       row :fecha_pase
       row :destino
     end
   end
 
   form do |f|
-    f.inputs do
-      f.input :alumno_id
+    a = Alumno.find(f.object.alumno_id) rescue nil
+    if a != nil 
+      li "#{a.id} - #{a.nombre} #{a.apellido}"
+    else
+      li ""
+    end
+    f.inputs do      
       f.input :fecha_pase
       f.input :destino
     end
