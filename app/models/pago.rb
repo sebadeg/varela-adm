be8a91b59a95c4b7de7f149fc888045c38af30a3
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-03 16:24:09 -0300

    Recargos

--- app/models/pago.rb
+++ app/models/pago.rb
@@ -34,14 +34,6 @@ class Pago < ApplicationRecord
         s.sheet(0).each do |line|
           if !primera
             archs.push( {
-
-#                fecha = Date.strptime(line[9], '%d/%m/%Y')
-#                cuenta = line[3].to_i
-#                dato = line[4]
-#                moneda = line[5]
-#                importe = line[6].to_f
-#                sucursal = "Sistarbanc: Subagencia " + line[7].to_i.to_s + " Caja " + line[8] + " Movimiento " + line[10] 
-
                 info: line.to_csv(row_sep: ";"),
                 fecha: Date.strptime(line[9], '%d/%m/%Y'),
                 cuenta: line[3].to_i,
@@ -61,14 +53,6 @@ class Pago < ApplicationRecord
         s.sheet(0).each do |line|
           if !primera
             archs.push( { 
-
-#                fecha = line[9]
-#                cuenta = line[3].to_i
-#                dato = line[4]
-#                moneda = line[5] == 0.0 ? 'UYU' : ''
-#                importe = line[6]
-#                sucursal = "Red Pagos: Subagencia " + line[7].to_i.to_s + " Caja " + line[8].to_i.to_s + " Movimiento " + line[10].to_i.to_s 
-
             	info: line.to_csv(row_sep: ";"), 
             	fecha: line[9],
             	cuenta: line[3].to_i,
@@ -88,12 +72,6 @@ class Pago < ApplicationRecord
         s.sheet(0).each do |line|
           if !primera
             archs.push( { 
-#                fecha = line[0]
-#                dato = line[2] + ',' + line[3]
-#                moneda = line[4] == '$' ? 'UYU' : ''
-#                importe = line[5]
-#                sucursal = "Red Pagos: Subagencia " + line[6].to_i.to_s + " Caja " + line[7].to_i.to_s + " Movimiento " + line[8].to_i.to_s 
-
             	info: line.to_csv(row_sep: ";"), 
             	fecha: line[0],
             	cuenta: "", 
@@ -114,15 +92,6 @@ class Pago < ApplicationRecord
       s.each do |line|
         ss = line.encode('UTF-8', invalid: :replace, :undef => :replace, replace: 'X').split(",")
         archs.push( { 
-
-#            fecha = Date.strptime(ss[7], '%d/%m/%Y')
-#            cuenta = ss[3].to_i
-#            dato = ss[1] + ',' + ss[2]
-#            factura = ss[0].to_i
-#            moneda = 'UYU'
-#            importe = ss[4].to_f/100
-#            sucursal = "Abitab: Agencia " + ss[5].to_i.to_s + " Subagencia " + ss[6].to_i.to_s 
-
         	info: line, 
         	fecha: Date.strptime(ss[7], '%d/%m/%Y'),
         	cuenta: ss[3].to_i, 
@@ -141,28 +110,13 @@ class Pago < ApplicationRecord
       if s[0][0,2] == "JP"
         s.each do |line|
 
-JP201901585598132810176920020190110002017692000011205012019
-JP201901585622135460098820020190110008014000000010310012019
-JP201901585576130000212900020190110008021290000047210012019
-JP201901585574129920185300020190110008018530000023710012019
-JP201901585607134030566120020190110000056612000019710012019
-JP201901585492121180158500020190110007015850000024510012019
-
-# #JP201712 489405 13810 00916400 20171231 006 00916400 00113 12122017
-# #JP201712489405138100091640020171231006009164000011312122017
+#JP201901 585622 13546 00988200 20190110 008 01400000 00103 10012019
+#JP201712 489405 13810 00916400 20171231 006 00916400 00113 12122017
           archs.push( { 
-
-#            fecha = Date.strptime(line[51,2]+'-'+line[53,2]+'-'+line[55,4], '%d-%m-%Y')
-#            cuenta = line[14,5].to_i
-#            factura = line[8,6].to_i
-#            moneda = 'UYU'
-#            importe = line[19,8].to_f/100
-#            sucursal = "Red Pagos: Agencia " + line[46,5].to_i.to_s + " Subagencia " + line[35,3].to_i.to_s 
-
             info: line, 
             fecha: Date.strptime(line[51,2]+'-'+line[53,2]+'-'+line[55,4], '%d-%m-%Y'),
             cuenta: line[14,5].to_i, 
-            importe: line[19,8].to_f/100,
+            importe: line[38- 19,8].to_f/100,
             dato: "",
             moneda: "",
             factura: line[8,6].to_i,
@@ -176,15 +130,6 @@ JP201901585492121180158500020190110007015850000024510012019
         s.each do |line|
           if ( line[0,2] == '1D' )
             archs.push( {
-
-#              fecha = Date.strptime(line[11,2]+'-'+line[9,2]+'-20'+line[7,2], '%d-%m-%Y')
-#              cuenta = line[13,5].to_i
-#              dato = line[80..(line.length-1)]
-#              factura = line[30,6].to_i
-#              moneda = 'UYU'
-#              importe = line[59,8].to_f/100
-#              sucursal = "BROU"
-
               info: line, 
               fecha: Date.strptime(line[11,2]+'-'+line[9,2]+'-20'+line[7,2], '%d-%m-%Y'),
               cuenta: line[13,5].to_i, 
