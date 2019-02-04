e327e73dbef35e60431b71d8604abe26923455e2
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-03 21:16:01 -0300

    Recargos

--- app/models/Ability.rb
+++ app/models/Ability.rb
@@ -32,7 +32,7 @@ class Ability
       end
 
       can :manage, InscripcionAlumno, InscripcionAlumno.where(
-        "alumno_id IN (SELECT alumno_id FROM lista_alumnos WHERE lista_id IN (SELECT id FROM listas WHERE sector_id IN (" + s + ") AND anio IN (SELECT anio FROM configs WHERE NOT anio IS NULL)))"
+        "alumno_id IN (SELECT alumno_id FROM lista_alumnos WHERE lista_id IN (SELECT id FROM listas WHERE sector_id IN (" + s + ") AND anio=2018))"
         ) do |x|
         true
       end
