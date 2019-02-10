38fd4a8c2c714d361a0eaca95483ad85c3201ff9
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-10 12:52:17 -0300

    Actividad

--- app/admin/ver_aactividads.rb
+++ app/admin/ver_aactividads.rb
@@ -2,18 +2,13 @@ ActiveAdmin.register_page "Ver_Aactividad" do
 
   menu priority: 10000, label: "Ver actividad", parent: "Actividad 2019"
 
-  member_action :ver, method: :post do  
-    redirect_to admin_ver_aactividad_path, notice: "HECHO"
-  end
+  page_action :ver, method: :post do
 
-  controller do
-    def ver
-      @alumno_id = params[:alumno_id]      
-    end
+    redirect_to action: admin_ver_aactividad_path, alumno_id: params[:alumno_id], notice: "HECHO"
   end
 
   content do
-    render partial: 'ver_aactividad', locals: { alumno_id: @alumno_id }
+    render partial: 'ver_aactividad', locals: { alumno_id: params[:alumno_id]  }
   end
 
 end
