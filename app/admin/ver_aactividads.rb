037155348cea46c14fb79e105cf2b31a9d2747b5
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-10 20:52:53 -0300

    Actividades

--- app/admin/ver_aactividads.rb
+++ app/admin/ver_aactividads.rb
@@ -2,29 +2,21 @@ ActiveAdmin.register_page "Ver_Aactividad" do
 
   menu priority: 10000, label: "Ver actividad", parent: "Actividad 2019"
 
-  page_action :ver, method: :post do
+  page_action :descargar, method: :post do
     p "//////////"
-    p "Ver 2"
-    p "//////////"
+    p "Descargar"
     p params
-
-    Temp.create(usuario_id: current_admin_usuario.id, temp_id: params[:alumno_id] )
-
+    p "//////////"
     redirect_to admin_ver_aactividad_path
-    
-    
+  end
+
+  page_action :ver, method: :post do
+    Temp.create(usuario_id: current_admin_usuario.id, temp_id: params[:alumno_id] )
+    redirect_to admin_ver_aactividad_path
   end
 
   content do
     temp = Temp.where("usuario_id = #{current_admin_usuario.id}").order(id: :desc).limit(1).first rescue nil
-
-    p "++++++++++"
-    p "++++++++++"
-    p temp.temp_id
-    p "++++++++++"
-    p "++++++++++"
-
-
     if temp == nil 
       render partial: 'ver_aactividad'
     else
