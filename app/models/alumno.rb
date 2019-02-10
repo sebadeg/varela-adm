3bb48c60e613dd2a0d68fb49147b3e70d18bd4dc
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-10 20:38:25 -0300

    Actividades

--- app/models/alumno.rb
+++ app/models/alumno.rb
@@ -25,6 +25,26 @@ class Alumno < ApplicationRecord
     return "IN (SELECT alumno_id FROM lista_alumnos WHERE lista_id IN (SELECT id FROM listas WHERE sector_id IN (" + s + ") AND anio IN (SELECT anio FROM configs WHERE NOT anio IS NULL)))"
   end
 
+  def self.sector(user)
+
+    s = ""
+    if user.primaria
+      s = "1"
+    end
+    if user.sec_mdeo
+      if s != ""
+        s = s + ","
+      end
+      s = s + "2"
+    end
+    if user.sec_cc
+      if s != ""
+        s = s + ","
+      end
+      s = s + "3"
+    end
+    return s
+  end
 
   def self.grado(id)
     if id!=nil
