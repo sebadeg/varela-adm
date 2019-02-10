f59acf710a5fcaf176178ba4934b4e14169cb33c
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-10 10:49:44 -0300

    Actividad

--- app/admin2/aactividad.rb
+++ app/admin2/aactividad.rb
@@ -3,15 +3,17 @@ ActiveAdmin.register_page "Aactividad" do
   menu priority: 10000, label: "Vista previa", parent: "Actividad 2019"
 
   page_action :ver, method: :post do  	
-   	@alumno_id = eval(params[:alumno])
-    redirect_to admin_aactividad_path, notice: @alumno_id.to_s
+   	#@alumno_id = eval(params[:alumno])
+    redirect_to admin_aactividad_path, notice: "HECHO"
   end
 
   content do
-    render partial: 'aactividad', locals:
-    {
-      alumno_id: @alumno_id
-    }
+    render partial: 'aactividad'
+
+    # , locals:
+    # {
+    #   alumno_id: @alumno_id
+    # }
   end
 
 end
