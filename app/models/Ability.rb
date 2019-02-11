c8f2646e95c809b273ca2c81094de9394f5396e8
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-10 21:39:39 -0300

    Actividades

--- app/models/Ability.rb
+++ app/models/Ability.rb
@@ -52,6 +52,12 @@ class Ability
         can :manage, Lista, Lista.where("sector_id IN (" + s + ") AND anio IN (SELECT anio FROM configs WHERE NOT anio IS NULL)") do |x|
           true
         end
+        can :manage, Aactividad
+        can :manage, AactividadAlumno
+        can :manage, AactividadOpcion
+        can :manage, AactividadLista
+        can :manage, AactividadArchivo
+
         can :manage, Inscripcion
         can :manage, Seguimiento
         can :manage, ActiveAdmin::Page, :name => "Seguimiento Cuenta"
