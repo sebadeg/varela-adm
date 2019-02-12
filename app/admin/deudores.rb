0c5e43677847a702e170b8b6f061e6cc72d9eb18
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-12 01:15:18 -0300

    Deudores

--- app/admin/deudores.rb
+++ app/admin/deudores.rb
@@ -21,28 +21,28 @@ ActiveAdmin.register_page "Deudores" do
       while fecha_desde < ultima_fecha do
 
         Mov.where("movgru=1 AND movcap=1 AND movrub=12 AND movsub=10 AND movfec>='#{fecha_desde.strftime('%Y-%m-%d')}' AND movfec<'#{fecha_hasta.strftime('%Y-%m-%d')}'").order(:movcta,:movfec).each do |m|
-          if ( !@saldo.has_key?(m.movcta) )
-            @saldo[m.movcta] = 0
-            deuda[m.movcta] = Array.new
-            fecha[m.movcta] = Array.new
+          if ( !@saldo.has_key?(m.movcta.to_i) )
+            @saldo[m.movcta.to_i] = 0
+            deuda[m.movcta.to_i] = Array.new
+            fecha[m.movcta.to_i] = Array.new
           end
 
           d = m.movdeb - m.movhab
 		  if d <= 0
-		  	@saldo[m.movcta] = @saldo[m.movcta] - d
+		  	@saldo[m.movcta.to_i] = @saldo[m.movcta.to_i] - d
 		  else
-		  	deuda[m.movcta].push(d)
-		  	fecha[m.movcta].push(m.movfec)
+		  	deuda[m.movcta.to_i].push(d)
+		  	fecha[m.movcta.to_i].push(m.movfec)
 		  end
 
-		  while @saldo[m.movcta] > 0 && deuda[m.movcta].count > 0 do
-		  	if deuda[m.movcta][0] <= @saldo[m.movcta]
-              @saldo[m.movcta] = @saldo[m.movcta] - deuda[m.movcta][0]
-              deuda[m.movcta].delete_at(0)
-              fecha[m.movcta].delete_at(0)
+		  while @saldo[m.movcta.to_i] > 0 && deuda[m.movcta.to_i].count > 0 do
+		  	if deuda[m.movcta.to_i][0] <= @saldo[m.movcta.to_i]
+              @saldo[m.movcta.to_i] = @saldo[m.movcta.to_i] - deuda[m.movcta.to_i][0]
+              deuda[m.movcta.to_i].delete_at(0)
+              fecha[m.movcta.to_i].delete_at(0)
             else
-              deuda[m.movcta][0] = deuda[m.movcta][0] - @saldo[m.movcta]
-              @saldo[m.movcta] = 0
+              deuda[m.movcta.to_i][0] = deuda[m.movcta.to_i][0] - @saldo[m.movcta.to_i]
+              @saldo[m.movcta.to_i] = 0
 		  	end		  	
 		  end
         end
@@ -52,28 +52,28 @@ ActiveAdmin.register_page "Deudores" do
       end
 
       Movimiento.where("id>=1300 AND fecha<='#{DateTime.now.strftime('%Y-%m-%d')}' AND (fecha>='2019-01-01' OR tipo=1005) ").order(:fecha).each do |m|
-        if ( !@saldo.has_key?(m.cuenta_id) )
-          @saldo[m.cuenta_id] = 0
-          deuda[m.cuenta_id] = Array.new
-          fecha[m.cuenta_id] = Array.new
+        if ( !@saldo.has_key?(m.cuenta_id.to_i) )
+          @saldo[m.cuenta_id.to_i] = 0
+          deuda[m.cuenta_id.to_i] = Array.new
+          fecha[m.cuenta_id.to_i] = Array.new
         end
         
         d = m.debe - m.haber
         if d <= 0
-          @saldo[m.cuenta_id] = @saldo[m.cuenta_id] - d
+          @saldo[m.cuenta_id.to_i] = @saldo[m.cuenta_id.to_i] - d
         else
-		  deuda[m.cuenta_id].push(d)
-		  fecha[m.cuenta_id].push(m.fecha)
+		  deuda[m.cuenta_id.to_i].push(d)
+		  fecha[m.cuenta_id.to_i].push(m.fecha)
 		end
 
-		while @saldo[m.cuenta_id] > 0 && deuda[m.cuenta_id].count > 0 do
-		  if deuda[m.cuenta_id][0] <= @saldo[m.cuenta_id]
-            @saldo[m.cuenta_id] = @saldo[m.cuenta_id] - deuda[m.cuenta_id][0]
-            deuda[m.cuenta_id].delete_at(0)
-            fecha[m.cuenta_id].delete_at(0)
+		while @saldo[m.cuenta_id.to_i] > 0 && deuda[m.cuenta_id.to_i].count > 0 do
+		  if deuda[m.cuenta_id.to_i][0] <= @saldo[m.cuenta_id.to_i]
+            @saldo[m.cuenta_id.to_i] = @saldo[m.cuenta_id.to_i] - deuda[m.cuenta_id.to_i][0]
+            deuda[m.cuenta_id.to_i].delete_at(0)
+            fecha[m.cuenta_id.to_i].delete_at(0)
           else
-            deuda[m.cuenta_id][0] = deuda[m.cuenta_id][0] - @saldo[m.cuenta_id]
-            @saldo[m.cuenta_id] = 0
+            deuda[m.cuenta_id.to_i][0] = deuda[m.cuenta_id.to_i][0] - @saldo[m.cuenta_id.to_i]
+            @saldo[m.cuenta_id.to_i] = 0
 		  end		  	
 		end
       end
