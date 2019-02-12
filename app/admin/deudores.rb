9dd7c4a1e45b3e7d31d657ad89c0bf36a2e87c0a
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-11 21:12:41 -0300

    Deudores

--- app/admin/deudores.rb
+++ app/admin/deudores.rb
@@ -16,7 +16,9 @@ ActiveAdmin.register_page "Deudores" do
           @ultimo[m.movcta] = Date.new(2013,12,31)
         end
         @saldo[m.movcta] = @saldo[m.movcta] + m.movdeb - m.movhab
-        @ultimo[m.movcta] = m.movfec
+        if @saldo[m.movcta] <= 0
+          @ultimo[m.movcta] = m.movfec
+        end
       end
       Mov.where("movgru=1 AND movcap=1 AND movrub=12 AND movsub=10 AND movfec>='2015-01-01' AND movfec<='2015-12-31'").order(:movcta,:movfec).each do |m|
         if ( !@saldo.has_key?(m.movcta) )
@@ -24,7 +26,9 @@ ActiveAdmin.register_page "Deudores" do
           @ultimo[m.movcta] = Date.new(2013,12,31)
         end
         @saldo[m.movcta] = @saldo[m.movcta] + m.movdeb - m.movhab
-        @ultimo[m.movcta] = m.movfec
+        if @saldo[m.movcta] <= 0
+          @ultimo[m.movcta] = m.movfec
+        end
       end
       Mov.where("movgru=1 AND movcap=1 AND movrub=12 AND movsub=10 AND movfec>='2016-01-01' AND movfec<='2016-12-31'").order(:movcta,:movfec).each do |m|
         if ( !@saldo.has_key?(m.movcta) )
@@ -32,7 +36,9 @@ ActiveAdmin.register_page "Deudores" do
           @ultimo[m.movcta] = Date.new(2013,12,31)
         end
         @saldo[m.movcta] = @saldo[m.movcta] + m.movdeb - m.movhab
-        @ultimo[m.movcta] = m.movfec
+        if @saldo[m.movcta] <= 0
+          @ultimo[m.movcta] = m.movfec
+        end
       end
       Mov.where("movgru=1 AND movcap=1 AND movrub=12 AND movsub=10 AND movfec>='2017-01-01' AND movfec<='2017-12-31'").order(:movcta,:movfec).each do |m|
         if ( !@saldo.has_key?(m.movcta) )
@@ -40,7 +46,9 @@ ActiveAdmin.register_page "Deudores" do
           @ultimo[m.movcta] = Date.new(2013,12,31)
         end
         @saldo[m.movcta] = @saldo[m.movcta] + m.movdeb - m.movhab
-        @ultimo[m.movcta] = m.movfec
+        if @saldo[m.movcta] <= 0
+          @ultimo[m.movcta] = m.movfec
+        end
       end
       Mov.where("movgru=1 AND movcap=1 AND movrub=12 AND movsub=10 AND movfec>='2018-01-01'").order(:movcta,:movfec).each do |m|
         if ( !@saldo.has_key?(m.movcta) )
@@ -48,7 +56,9 @@ ActiveAdmin.register_page "Deudores" do
           @ultimo[m.movcta] = Date.new(2013,12,31)
         end
         @saldo[m.movcta] = @saldo[m.movcta] + m.movdeb - m.movhab
-        @ultimo[m.movcta] = m.movfec
+        if @saldo[m.movcta] <= 0
+          @ultimo[m.movcta] = m.movfec
+        end
       end
   	end
   end
