bf68dfdfc6b8639cc1bab718273eb5d003502eaf
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-16 12:24:26 -0300

    Cuentas

--- app/admin/movimientos.rb
+++ app/admin/movimientos.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register Movimiento do
 
-  config.sort_order = 'fecha'
+  #config.sort_order = 'fecha'
 
 
   menu label: 'Movimientos'
   menu parent: 'Cuenta Corriente'
 
+  saldo = 0
 
   index do
   	#selectable_column
@@ -16,32 +17,9 @@ ActiveAdmin.register Movimiento do
     column "Descripción", :descripcion
     column "Debe", :debe
     column "Haber", :haber
-    #if params[:cuenta_id]!=nil 
-      column "Saldo", :saldo
-    #end
+    column "Saldo" {|mov| saldo = saldo + mov.debe - mov.haber}
   end
 
   filter :cuenta_id
-
-  controller do
-
-    def index
-  #    if params[:cuenta_id]==nil 
-  #     return
-  #    end
-
-  #    ActiveRecord::Base.connection.execute( "UPDATE movimientos SET saldo=nil WHERE cuenta_id=#{params[:cuenta_id]};" )
-
-
-  #     saldo = 0;
-  #     Movimiento.where( "cuenta_id=#{params[:cuenta_id]} AND fecha<='#{DateTime.now.strftime('%Y-%m-%d'}'").order(fecha: :asc).each do |mov|
-  #       saldo = saldo + mov.debe - mov.haber
-  #       mov.saldo = saldo
-  #       mov.update!
-  #     end
-
-     end 
-
-  end
 
 end
