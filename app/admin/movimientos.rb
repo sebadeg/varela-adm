fa25d8e6b1024f75d5f151243ca2f55c7117b0ed
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-16 13:12:16 -0300

    Cuentas

--- app/admin/movimientos.rb
+++ app/admin/movimientos.rb
@@ -8,7 +8,8 @@ ActiveAdmin.register Movimiento do
 
   before_action :reset_saldo, only: [:index]
 
-  @saldo = 0
+  saldo = 0
+  cuenta = nil
 
   index do
   	#selectable_column
@@ -19,18 +20,27 @@ ActiveAdmin.register Movimiento do
     column "Descripción", :descripcion
     column "Debe", :debe
     column "Haber", :haber
-    column "Saldo" do |mov| mov.cuenta_id == 12121 ? @saldo = @saldo + mov.debe - mov.haber : "" end
+    column "Saldo" do |mov| mov.cuenta_id == 12121 ? saldo = saldo + mov.debe - mov.haber : "" end
   end
 
   filter :cuenta_id
 
   controller do    
     def reset_saldo
-      @saldo = 0
+      saldo = 0
     end
 
     def index
+      p "-----------"
+      p "-----------"
+      p "-----------"
+      p "-----------"
       p params
+      p cuenta
+      p "-----------"
+      p "-----------"
+      p "-----------"
+      p "-----------"
     end
     #   if params[:cuenta_id_equals] != nil
     #     cuenta = params[:cuenta_id_equals].to_i
