ec04a3a091d95b227632bb843cc4303f71ff0b73
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-23 09:46:29 -0300

    Especiales

--- app/models/especial_alumno.rb
+++ app/models/especial_alumno.rb
@@ -1,2 +1,5 @@
 class EspecialAlumno < ApplicationRecord
+  belongs_to :especial
+  belongs_to :alumno
+
 end
--- app/models/especial_cuenta.rb
+++ app/models/especial_cuenta.rb
@@ -1,2 +1,4 @@
 class EspecialCuenta < ApplicationRecord
+  belongs_to :especial
+  belongs_to :cuenta
 end
