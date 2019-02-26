682de32f9a6435a51a4e6e49b6cdc029291ebcbf
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-26 20:13:14 -0300

    Contratos

--- app/admin/contratos.rb
+++ app/admin/contratos.rb
@@ -14,103 +14,103 @@ ActiveAdmin.register Contrato do
 
   permit_params :id, :nombre, :cuenta_id, :alumno_id, :cuotas,:aguinaldos,:comienzo,:importe,:concepto_id
 
-  /#,      contrato_cuota_attributes: [:id,:contrato_id,:fecha,:precio,:descuento,:_destroy]
+  # /#,      contrato_cuota_attributes: [:id,:contrato_id,:fecha,:precio,:descuento,:_destroy]
 
-  menu priority: 31, label: "Contrato"
+  # menu priority: 31, label: "Contrato"
 
-  action_item :actualizar, only: :show do
-    link_to "Actualizar", actualizar_admin_contrato_path(contrato), method: :put 
-  end
+  # action_item :actualizar, only: :show do
+  #   link_to "Actualizar", actualizar_admin_contrato_path(contrato), method: :put 
+  # end
 
-  action_item :actualizarfunc, only: :show do
-    link_to "Actualizar Func", actualizarfunc_admin_contrato_path(contrato), method: :put 
-  end
+  # action_item :actualizarfunc, only: :show do
+  #   link_to "Actualizar Func", actualizarfunc_admin_contrato_path(contrato), method: :put 
+  # end
 
-  member_action :actualizar, method: :put do
-    id = params[:id]
+  # member_action :actualizar, method: :put do
+  #   id = params[:id]
 
-    Contrato.where("id>183").each do |c|
+  #   Contrato.where("id>183").each do |c|
   
-      c6 = ContratoCuota.where(["contrato_id=? AND fecha='2018-06-01'",c.id]).first rescue nil
-      c7 = ContratoCuota.where(["contrato_id=? AND fecha='2018-07-01'",c.id]).first rescue nil
-      c8 = ContratoCuota.where(["contrato_id=? AND fecha='2018-08-01'",c.id]).first rescue nil
-      c9 = ContratoCuota.where(["contrato_id=? AND fecha='2018-09-01'",c.id]).first rescue nil
-      c10 = ContratoCuota.where(["contrato_id=? AND fecha='2018-10-01'",c.id]).first rescue nil
-      c11 = ContratoCuota.where(["contrato_id=? AND fecha='2018-11-01'",c.id]).first rescue nil
-      c12 = ContratoCuota.where(["contrato_id=? AND fecha='2018-12-01'",c.id]).first rescue nil
+  #     c6 = ContratoCuota.where(["contrato_id=? AND fecha='2018-06-01'",c.id]).first rescue nil
+  #     c7 = ContratoCuota.where(["contrato_id=? AND fecha='2018-07-01'",c.id]).first rescue nil
+  #     c8 = ContratoCuota.where(["contrato_id=? AND fecha='2018-08-01'",c.id]).first rescue nil
+  #     c9 = ContratoCuota.where(["contrato_id=? AND fecha='2018-09-01'",c.id]).first rescue nil
+  #     c10 = ContratoCuota.where(["contrato_id=? AND fecha='2018-10-01'",c.id]).first rescue nil
+  #     c11 = ContratoCuota.where(["contrato_id=? AND fecha='2018-11-01'",c.id]).first rescue nil
+  #     c12 = ContratoCuota.where(["contrato_id=? AND fecha='2018-12-01'",c.id]).first rescue nil
 
-      if c6 != nil && c7 != nil 
-        if c6.precio != c7.precio
-          if c8 != nil && c8.precio == c6.precio && c8.descuento == c6.descuento
-            if c12 != nil 
-              if c8.precio == c12.precio && c8.descuento == c12.descuento
-                c12.precio = c7.precio
-                c12.descuento = c7.descuento
-              else
-                c12.precio = (c7.precio*1.5).round(0)
-                c12.descuento = (c7.descuento*1.5).round(0)
-              end
-              c12.save!
-            end
+  #     if c6 != nil && c7 != nil 
+  #       if c6.precio != c7.precio
+  #         if c8 != nil && c8.precio == c6.precio && c8.descuento == c6.descuento
+  #           if c12 != nil 
+  #             if c8.precio == c12.precio && c8.descuento == c12.descuento
+  #               c12.precio = c7.precio
+  #               c12.descuento = c7.descuento
+  #             else
+  #               c12.precio = (c7.precio*1.5).round(0)
+  #               c12.descuento = (c7.descuento*1.5).round(0)
+  #             end
+  #             c12.save!
+  #           end
 
-            c8.precio = c7.precio
-            c8.descuento = c7.descuento
-            c8.save!
+  #           c8.precio = c7.precio
+  #           c8.descuento = c7.descuento
+  #           c8.save!
 
-            if c9 != nil && c9.precio == c6.precio && c9.descuento == c6.descuento
-              c9.precio = c7.precio
-              c9.descuento = c7.descuento
-              c9.save!
-            end
-            if c10 != nil && c10.precio == c6.precio && c10.descuento == c6.descuento
-              c10.precio = c7.precio
-              c10.descuento = c7.descuento
-              c10.save!
-            end
-            if c11 != nil && c11.precio == c6.precio && c11.descuento == c6.descuento
-              c11.precio = c7.precio
-              c11.descuento = c7.descuento
-              c11.save!
-            end
-          end
-          ContratoCuota.where(["contrato_id=? AND fecha>'2018-12-01'",c.id]).each do |cc|
-            if cc.precio == c6.precio && cc.descuento == c6.descuento
-              cc.precio = c7.precio
-              cc.descuento = c7.descuento
-              cc.save!
-            end
-          end
+  #           if c9 != nil && c9.precio == c6.precio && c9.descuento == c6.descuento
+  #             c9.precio = c7.precio
+  #             c9.descuento = c7.descuento
+  #             c9.save!
+  #           end
+  #           if c10 != nil && c10.precio == c6.precio && c10.descuento == c6.descuento
+  #             c10.precio = c7.precio
+  #             c10.descuento = c7.descuento
+  #             c10.save!
+  #           end
+  #           if c11 != nil && c11.precio == c6.precio && c11.descuento == c6.descuento
+  #             c11.precio = c7.precio
+  #             c11.descuento = c7.descuento
+  #             c11.save!
+  #           end
+  #         end
+  #         ContratoCuota.where(["contrato_id=? AND fecha>'2018-12-01'",c.id]).each do |cc|
+  #           if cc.precio == c6.precio && cc.descuento == c6.descuento
+  #             cc.precio = c7.precio
+  #             cc.descuento = c7.descuento
+  #             cc.save!
+  #           end
+  #         end
 
-        end
-      end
-    end
+  #       end
+  #     end
+  #   end
 
-    contrato = Contrato.find(id.to_i+1)
-    redirect_to admin_contrato_path(contrato)
-  end
+  #   contrato = Contrato.find(id.to_i+1)
+  #   redirect_to admin_contrato_path(contrato)
+  # end
 
-  member_action :actualizarfunc, method: :put do
-    id = params[:id]
+  # member_action :actualizarfunc, method: :put do
+  #   id = params[:id]
 
-    cc = ContratoCuota.where(["contrato_id=? AND fecha='2018-07-01'",id]).first!
-    ContratoCuota.where(["contrato_id=? AND fecha>'2018-07-01' AND fecha<'2018-12-01'",id]).each do |x|
-      x.precio = cc.precio
-      x.descuento = cc.descuento
-      x.save!
-    end
+  #   cc = ContratoCuota.where(["contrato_id=? AND fecha='2018-07-01'",id]).first!
+  #   ContratoCuota.where(["contrato_id=? AND fecha>'2018-07-01' AND fecha<'2018-12-01'",id]).each do |x|
+  #     x.precio = cc.precio
+  #     x.descuento = cc.descuento
+  #     x.save!
+  #   end
 
-    ContratoCuota.where(["contrato_id=? AND fecha='2018-12-01'",id]).each do |x|
-      x.precio = (cc.precio*1.5).round(0)
-      x.descuento = (cc.descuento*1.5).round(0)
-      x.save!
-    end
+  #   ContratoCuota.where(["contrato_id=? AND fecha='2018-12-01'",id]).each do |x|
+  #     x.precio = (cc.precio*1.5).round(0)
+  #     x.descuento = (cc.descuento*1.5).round(0)
+  #     x.save!
+  #   end
 
-    # ActiveRecord::Base.connection.execute( "DELETE FROM actividad_alumnos WHERE actividad_id=#{id};" )
-    # ActiveRecord::Base.connection.execute( "INSERT INTO actividad_alumnos (actividad_id,alumno_id,created_at,updated_at) (SELECT #{id},id,now(),now() FROM alumnos WHERE id IN (SELECT alumno_id FROM lista_alumnos WHERE lista_id IN (SELECT lista_id FROM actividad_listas WHERE actividad_id=#{id})));" )
+  #   # ActiveRecord::Base.connection.execute( "DELETE FROM actividad_alumnos WHERE actividad_id=#{id};" )
+  #   # ActiveRecord::Base.connection.execute( "INSERT INTO actividad_alumnos (actividad_id,alumno_id,created_at,updated_at) (SELECT #{id},id,now(),now() FROM alumnos WHERE id IN (SELECT alumno_id FROM lista_alumnos WHERE lista_id IN (SELECT lista_id FROM actividad_listas WHERE actividad_id=#{id})));" )
 
-    contrato = Contrato.find(id.to_i+1)
-    redirect_to admin_contrato_path(contrato)
-  end#/
+  #   contrato = Contrato.find(id.to_i+1)
+  #   redirect_to admin_contrato_path(contrato)
+  # end#/
 
   index do
     #selectable_column
@@ -138,13 +138,13 @@ ActiveAdmin.register Contrato do
       row :comienzo
       row :importe
       row :concepto_id
-      /#row "Cuotas" do 
-        table_for ContratoCuota.where("contrato_id=#{r.id}").order(:fecha) do |t|
-          t.column :fecha
-          t.column :precio
-          t.column :descuento
-        end
-      end#/
+      # /#row "Cuotas" do 
+      #   table_for ContratoCuota.where("contrato_id=#{r.id}").order(:fecha) do |t|
+      #     t.column :fecha
+      #     t.column :precio
+      #     t.column :descuento
+      #   end
+      # end#/
     end
   end
 
@@ -160,13 +160,13 @@ ActiveAdmin.register Contrato do
       f.input :importe
     end
 
-    /#f.inputs do
-      f.has_many :contrato_cuota, heading: "Cuotas", allow_destroy: true, new_record: true do |l|
-        l.input :fecha
-        l.input :precio
-        l.input :descuento
-      end
-    end#/
+    # /#f.inputs do
+    #   f.has_many :contrato_cuota, heading: "Cuotas", allow_destroy: true, new_record: true do |l|
+    #     l.input :fecha
+    #     l.input :precio
+    #     l.input :descuento
+    #   end
+    # end#/
     f.actions
   end
 
