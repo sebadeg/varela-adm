119f649bc9f6df6bd57284b78a6c6ff46061d16e
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-02-27 00:34:25 -0300

    Autorizaciones

--- app/admin/autorizaciones.rb
+++ app/admin/autorizaciones.rb
@@ -54,7 +54,12 @@ ActiveAdmin.register Actividad, :as => 'Autorizaciones' do
 
     f.inputs do
       f.has_many :actividad_alumno, heading: "Alumnos", allow_destroy: true, new_record: true do |l|
-        l.label Alumno.find(l.object.alumno_id).nombre + " " + Alumno.find(l.object.alumno_id).apellido
+        
+        alumno = Alumno.find(l.object.alumno_id) rescue nil
+        if alumno != nil
+          l.label alumno.nombre + " " + alumno.apellido
+        end
+        
       # t.column "Opcion" do |x| ActividadAlumno.find_by(actividad_id: r.id, alumno_id: x.id).opcion end
       # t.column "Fecha" do |x| ActividadAlumno.find_by(actividad_id: r.id, alumno_id: x.id).fecha end
       # t.column "Secretaría" do |x| ActividadAlumno.find_by(actividad_id: r.id, alumno_id: x.id).secretaria end
