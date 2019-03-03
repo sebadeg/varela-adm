699214956558c0483640bbb03c289ac8319338e1
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-03-03 15:35:35 -0300

    Crear

--- app/admin/actividades.rb
+++ app/admin/actividades.rb
@@ -151,7 +151,7 @@ ActiveAdmin.register Actividad do
     end
 
     f.inputs do
-      f.has_many :actividad_archivo, heading: "Archivos", sortable: :indice, allow_destroy: true, new_record: true do |l|
+      f.has_many :actividad_archivo, heading: "Archivos", allow_destroy: true, new_record: true do |l|
         l.input :indice
         if l.object == nil || l.object.new_record?
           l.input :nombre, :input_html => { :value => "" }, as: :hidden
