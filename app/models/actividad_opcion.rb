ece25dc8378c36f469a1fae8e591e7394f235033
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-03-03 11:59:19 -0300

    Crear

--- app/models/actividad_opcion.rb
+++ app/models/actividad_opcion.rb
@@ -5,7 +5,11 @@ class ActividadOpcion < ApplicationRecord
     opciones = Array.new
     elecciones = Hash.new
 
-    ActividadOpcion.where(["actividad_id=#{actividad_id}"]).order(:id).each do |opcion|
+    p "------------------------------------------"
+    p actividad.id
+    p "------------------------------------------"
+
+    ActividadOpcion.where(["actividad_id=#{self.actividad_id}"]).order(:id).each do |opcion|
       
       s = opcion.concepto
       if ( opcion.cuotas > 0 )
