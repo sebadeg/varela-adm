9ef6237296e1ea355cbee9334427e904a2e8dc33
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-03-03 11:50:22 -0300

    Crear

--- app/models/actividad_opcion.rb
+++ app/models/actividad_opcion.rb
@@ -1,3 +1,25 @@
 class ActividadOpcion < ApplicationRecord
   belongs_to :actividad
+
+  def opciones
+    opciones = Array.new
+    elecciones = Hash.new
+
+    ActividadOpcion.where(["actividad_id=#{actividad_id}"]).order(:id).each do |opcion|
+      
+      s = opcion.concepto
+      if ( opcion.cuotas > 0 )
+        if ( opcion.cuotas == 1 )
+          s = s + " a debitar contado de $U "
+        else
+          s = s + " a debitar en #{opcion.cuotas} cuotas de $U "
+        end
+        s = s + "#{opcion.importe}"
+      end
+      opciones.push( [s,opcion.id] )
+      elecciones[opcion.id] = s
+    end
+
+    return { :opciones => opciones, :elecciones => elecciones }
+  end
 end
