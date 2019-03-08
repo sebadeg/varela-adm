2a3a70a73190c80a14cc4e9896190cff7d90e655
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-03-08 20:55:11 -0300

    Mails

--- app/admin/pendiente.rb
+++ app/admin/pendiente.rb
@@ -3,40 +3,7 @@ ActiveAdmin.register_page "Pendiente" do
   menu priority: 70, label: "Pendiente"
 
 
-  def log_exception(e, args = {})
-    extra_info = args[:info]
-
-    Rails.logger.error extra_info if extra_info
-    Rails.logger.error e.message
-    st = e.backtrace.join("\n")
-    Rails.logger.error st
-
-    extra_info ||= "<NO DETAILS>"
-    request = args[:request]
-    env = request ? request.env : {}
-    ExceptionNotifier::Notifier.exception_notification(env, e, :data => {:message => "Exception: #{extra_info}"}).deliver
-  end
-
-  def try_delivering_email(options = {}, &block)
-    begin
-      yield
-      return true
-    rescue  EOFError,
-            IOError,
-            TimeoutError,
-            Errno::ECONNRESET,
-            Errno::ECONNABORTED,
-            Errno::EPIPE,
-            Errno::ETIMEDOUT,
-            Net::SMTPAuthenticationError,
-            Net::SMTPServerBusy,
-            Net::SMTPSyntaxError,
-            Net::SMTPUnknownError,
-            OpenSSL::SSL::SSLError => e
-      log_exception(e, options)
-      return false
-    end
-  end
+ 
  
   page_action :enviarfactura, method: :post do
 
@@ -476,6 +443,44 @@ ActiveAdmin.register_page "Pendiente" do
     render partial: 'pendiente'
   end
 
+  controller do
+
+     def log_exception(e, args = {})
+    extra_info = args[:info]
+
+    Rails.logger.error extra_info if extra_info
+    Rails.logger.error e.message
+    st = e.backtrace.join("\n")
+    Rails.logger.error st
+
+    extra_info ||= "<NO DETAILS>"
+    request = args[:request]
+    env = request ? request.env : {}
+    ExceptionNotifier::Notifier.exception_notification(env, e, :data => {:message => "Exception: #{extra_info}"}).deliver
+  end
+
+  def try_delivering_email(options = {}, &block)
+    begin
+      yield
+      return true
+    rescue  EOFError,
+            IOError,
+            TimeoutError,
+            Errno::ECONNRESET,
+            Errno::ECONNABORTED,
+            Errno::EPIPE,
+            Errno::ETIMEDOUT,
+            Net::SMTPAuthenticationError,
+            Net::SMTPServerBusy,
+            Net::SMTPSyntaxError,
+            Net::SMTPUnknownError,
+            OpenSSL::SSL::SSLError => e
+      log_exception(e, options)
+      return false
+    end
+  end
+  
+  end
 
 
 #  page_action :validar, method: :post do  	
