596b503725cb132d068db177df2fd1ac6f77ae9c
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-03-09 15:13:26 -0300

    Actividad

--- app/models/actividad_opcion.rb
+++ app/models/actividad_opcion.rb
@@ -7,22 +7,23 @@ class ActividadOpcion < ApplicationRecord
     elecciones = Hash.new
 
     ActividadOpcion.where(["actividad_id=#{actividad_id}"]).order(:indice).each do |opcion|
-     
-      s = ""
-      if opcion.opcion_concepto_id != nil  
-        concepto = OpcionConcepto.find(opcion.opcion_concepto_id)
-        s = concepto != nil ? concepto.nombre : ""
-      end
-      if ( opcion.cuotas != nil && opcion.cuotas > 0 )
-        if ( opcion.cuotas == 1 )
-          s = s + " a debitar contado de $U "
-        else
-          s = s + " a debitar en #{opcion.cuotas} cuotas de $U "
+      if (opcion.fecha >= DateTime.now )
+        s = ""
+        if opcion.opcion_concepto_id != nil  
+          concepto = OpcionConcepto.find(opcion.opcion_concepto_id)
+          s = concepto != nil ? concepto.nombre : ""
         end
-        s = s + "#{opcion.importe}"
+        if ( opcion.cuotas != nil && opcion.cuotas > 0 )
+          if ( opcion.cuotas == 1 )
+            s = s + " a debitar contado de $U "
+          else
+            s = s + " a debitar en #{opcion.cuotas} cuotas de $U "
+          end
+          s = s + "#{opcion.importe}"
+        end
+        opciones.push( [s,opcion.id] )
+        elecciones[opcion.id] = s
       end
-      opciones.push( [s,opcion.id] )
-      elecciones[opcion.id] = s
     end
 
     return { :opciones => opciones, :elecciones => elecciones }
