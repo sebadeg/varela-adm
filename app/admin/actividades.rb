eb038d9dbd389cdc99437d0c0fba4a65f42305c0
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-03-09 17:30:50 -0300

    Actividad

--- app/admin/actividades.rb
+++ app/admin/actividades.rb
@@ -261,26 +261,27 @@ ActiveAdmin.register Actividad do
             p "----------"
 
             nombre = params[:actividad][:nombre]
+            id = params[:actividad][:actividad_alumno_attributes][i.to_s][:id]
+            secretaria = params[:actividad][:actividad_alumno_attributes][i.to_s][:opcion_secretaria]
 
-            actividad_alumno = ActividadAlumno.find(params[:actividad][:actividad_alumno_attributes][i.to_s][:id])
-            cuenta_id = CuentaAlumno.where(alumno_id: actividad_alumno.alumno_id).first.cuenta_id
-
-            secretaria = params[:actividad][:actividad_alumno_attributes][i.to_s][:opcion_secretaria]            
-            actividad_opcion=ActividadOpcion.find(secretaria)
-            fecha = DateTime.new(actividad_opcion.fecha.year,actividad_opcion.fecha.month,1)
-
-            if actividad_opcion.cuotas >= 1 
-              (1..actividad_opcion.cuotas).each do |cuota|
-                Movimiento.create(cuenta_id: cuenta_id, alumno: actividad_alumno.alumno_id, fecha: fecha + (cuota-1).month,
-                  descripcion: "#{nombre} #{cuota}/#{actividad_opcion.cuotas}" , extra: "", debe: actividad_opcion.importe,
-                  haber: 0, tipo: 1002, actividad_alumno_id: actividad_alumno.id, actividad_alumno_opcion: secretaria )
-              end
+            if id != nil && secretaria != nil
+              p Movimiento.where("actividad_alumno_id=#{id} AND actividad_alumno_opcion<>#{secretaria}").count
             end
-            # if id != nil && secretaria != nil 
-            #   Movimiento.where("alumno=#{alumno_id} AND actividad_alumno_id=#{id} AND actividad_alumno_opcion<>#{secretaria}").select(:alumno).distinct.each do |mov|
-            #     p mov.alumno                
+
+            # actividad_alumno = ActividadAlumno.find(id)
+            # cuenta_id = CuentaAlumno.where(alumno_id: actividad_alumno.alumno_id).first.cuenta_id
+
+            # actividad_opcion=ActividadOpcion.find(secretaria)
+            # fecha = DateTime.new(actividad_opcion.fecha.year,actividad_opcion.fecha.month,1)
+
+            # if actividad_opcion.cuotas >= 1 
+            #   (1..actividad_opcion.cuotas).each do |cuota|
+            #     Movimiento.create(cuenta_id: cuenta_id, alumno: actividad_alumno.alumno_id, fecha: fecha + (cuota-1).month,
+            #       descripcion: "#{nombre.upcase} #{cuota}/#{actividad_opcion.cuotas}" , extra: "", debe: actividad_opcion.importe,
+            #       haber: 0, tipo: 1002, actividad_alumno_id: actividad_alumno.id, actividad_alumno_opcion: secretaria )
             #   end
             # end
+
             p "----------"
             p "----------"
             p "----------"
