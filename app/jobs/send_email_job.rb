7ef54c4ff386728246617fc9c1e559f3748efa93
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-03-10 22:57:05 -0300

    Actividad

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -7,7 +7,7 @@ class SendEmailJob < ApplicationJob
       cuenta_id = factura.cuenta_id
       file_path = Rails.root.join("temp", "factura_#{cuenta_id}_#{factura.id}.pdf")
       factura.imprimir(file_path,cuenta_id,factura)
-      usuarios = Usuario.where( "id IN (SELECT usuario_id FROM titular_cuentas WHERE cuenta_id=#{cuenta_id})").first rescue nil
+      usuarios = Usuario.where( "id IN (SELECT usuario_id FROM titular_cuentas WHERE cuenta_id=#{cuenta_id})") rescue nil
       if ( usuarios != nil )
         usuarios.each do |usuario|
           UserMailer.facturacion( usuario, "Marzo 2019", cuenta_id, "factura_#{cuenta_id}_#{factura.id}.pdf", file_path ).deliver_now
