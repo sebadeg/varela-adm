b4a12f18940e14831b43be11619c8c26cee8050c
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-03-18 21:05:59 -0300

    Cuentas

--- app/admin/deudores.rb
+++ app/admin/deudores.rb
@@ -109,23 +109,24 @@ ActiveAdmin.register Deudor do
 
   filter :cuenta_id
 
-  # csv do
-  #   column :cuenta_id
-  #   column "Nombre" do |c|
-  #     if c.cuenta != nil
-  #       c.cuenta.nombre 
-  #     end
-  #   column "Convenio" do |c|
-  #     if c.cuenta != nil
-  #       c.cuenta.convenio 
-  #     end
-  #   end
-  #   column "> 360", :deuda360
-  #   column "> 180", :deuda180
-  #   column "> 90", :deuda90
-  #   column "> 60", :deuda60
-  #   column "> 30", :deuda30
-  #   column "> 0", :deuda0
-  # end
+  csv do
+    column :cuenta_id
+    column "Nombre" do |c|
+      if c.cuenta != nil
+        c.cuenta.nombre 
+      end
+    end
+    column "Convenio" do |c|
+      if c.cuenta != nil
+        c.cuenta.convenio 
+      end
+    end
+    column "> 360", :deuda360
+    column "> 180", :deuda180
+    column "> 90", :deuda90
+    column "> 60", :deuda60
+    column "> 30", :deuda30
+    column "> 0", :deuda0
+  end
 
 end
