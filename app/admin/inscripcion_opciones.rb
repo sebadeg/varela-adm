991bb60d0e980883971b32c88edb48d54a83ea96
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-09 10:01:11 -0300

    Insc

---
+++ app/admin/inscripcion_opciones.rb
@@ -0,0 +1,44 @@
+ActiveAdmin.register InscripcionOpcion do
+
+  menu label: 'Inscripción Opción', priority: 22 
+
+  permit_params :id, :nombre
+
+  index do
+  	#selectable_column
+    column :nombre
+    column :anio
+    column "Tipo" do |r| (r.inscripcion_opcion_tipo != nil ? "#{r.inscripcion_opcion_tipo.nombre}" : "" ) end
+    column :fecha
+    column :valor
+    column :formato
+
+    actions
+  end
+
+  filter :nombre
+
+  show do
+    attributes_table do
+      row :nombre
+      row :anio
+      row "Tipo" do |r| (r.inscripcion_opcion_tipo != nil ? "#{r.inscripcion_opcion_tipo.nombre}" : "" ) end
+      row :fecha
+      row :valor
+      row :formato
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :nombre
+      f.input :anio
+      f.input :inscripcion_opcion_tipo_id, :as => :select, :collection => InscripcionOpcionTipo.all.order(:nombre).map{|u| ["#{u.nombre}",u.id]}
+      f.input :fecha
+      f.input :valor
+      f.input :formato
+    end
+    f.actions
+  end
+
+end
--- app/models/inscripcion_opcion.rb
+++ app/models/inscripcion_opcion.rb
@@ -1,2 +1,3 @@
 class InscripcionOpcion < ApplicationRecord
+  belongs_to :inscripcion_opcion_tipo
 end
