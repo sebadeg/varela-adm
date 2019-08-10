34f7ef74669640529ed103230d7172767ef9f335
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-10 14:40:34 -0300

    Inscr

--- app/models/formulario.rb
+++ app/models/formulario.rb
@@ -2,6 +2,8 @@ class Formulario < ApplicationRecord
   has_many :formulario_inscripcion_opcion, :dependent => :delete_all
   accepts_nested_attributes_for :formulario_inscripcion_opcion, allow_destroy: true
 
+  belongs_to :proximo_grado
+
   scope :todos, -> { all }
   scope :corrientes, -> { where("anio IN (SELECT anio_inscripciones FROM configs WHERE NOT anio_inscripciones IS NULL)") }
 
