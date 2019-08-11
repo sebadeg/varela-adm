8f4393fbc86bbc4090b4bf1002f198977d71be7e
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-11 13:46:09 -0300

    Inscr

--- app/admin/03_facturacion/ejecutar.rb
+++ app/admin/03_facturacion/ejecutar.rb
@@ -14,35 +14,40 @@ ActiveAdmin.register_page "Ejecutar" do
 
   page_action :brou, method: :post do   
 
-    fecha = Config.find(1).fecha_facturacion
+    fecha_facturacion = Config.find(1).fecha_facturacion
+    fecha_vencimiento = fecha_facturacion + 9.days
+
+    if fecha_vencimiento.wday == 0
+      fecha_vencimiento = fecha_vencimiento + 1.days
+    elsif fecha_vencimiento.wday == 6
+      fecha_vencimiento = fecha_vencimiento + 2.days
+    end
+
+    titulo = "FACTURACION #{fecha_facturacion.strftime("%b").upcase}/#{fecha_facturacion.strftime("%Y")}"
+    dia = fecha_vencimiento.day
+    mes = fecha_vencimiento.month
+    anio = fecha_vencimiento.year
+
+    cantidad = 0
+    suma = 0
 
     file_name = "brou.txt"
     file = Tempfile.new(file_name)    
     File.open(file, "w+") do |f|
       f.write("#{fecha}\r\n")
 
+       Factura.where("cuenta_id IN (SELECT id FROM cuentas WHERE brou) AND fecha='#{fecha_facturacion}'").each do |x|
+         cuenta = x.cuenta_id
+         nombre = Cuenta.where("id=#{x.cuenta_id}").first.nombre
+         importe = x.total
+         factura = x.id
 
-
-       # vencimiento = "12/08/2019" #no puede ser sabado
-       # inicio = "01/08/2019"        
-       # titulo = "FACTURACION AGO/2019"
-       # dia = 12           #no puede ser sabado
-       # mes = 8
-       # anio = 2019
-       # cantidad = 0
-       # suma = 0
-       # Factura.where("cuenta_id IN (SELECT id FROM cuentas WHERE brou) AND fecha='2019-08-01'").each do |x|   #11869,11601,11624,11795,12037,12776,13857
-       #   cuenta = x.cuenta_id
-       #   nombre = Cuenta.where("id=#{x.cuenta_id}").first.nombre
-       #   importe = x.total
-       #   factura = x.id
-
-       #   str = ("#{nombre} -DEB.AUT.BROU" + ' ' * 48)[0,48]
-       #   suma = suma + importe 
-       #   cantidad = cantidad+1
-       #   f.write("1 00100#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}#{dia.to_s.rjust(2, "0")}#{cuenta}000000000000450798A00000000000#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}00000#{(importe*100).round(0).to_i.to_s.rjust(10, "0")}0000000000000#{str}0000000000000000000000000000000000000000000000000000000000\r\n" )
-       # end
-       # f.write("2 00100#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}#{dia.to_s.rjust(2, "0")}0000#{cantidad.to_s.rjust(2, "0")}00000000#{(suma*100).round(0).to_i.to_s.rjust(10, "0")}00000000000000000000000000000000000000000000000000000000000000000000000000000000111111111110000000000000000000000000000000000000000000000000000000000\r\n")
+         str = ("#{nombre} -DEB.AUT.BROU" + ' ' * 48)[0,48]
+         suma = suma + importe 
+         cantidad = cantidad+1
+         f.write("1 00100#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}#{dia.to_s.rjust(2, "0")}#{cuenta}000000000000450798A00000000000#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}00000#{(importe*100).round(0).to_i.to_s.rjust(10, "0")}0000000000000#{str}0000000000000000000000000000000000000000000000000000000000\r\n" )
+       end
+       f.write("2 00100#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}#{dia.to_s.rjust(2, "0")}0000#{cantidad.to_s.rjust(2, "0")}00000000#{(suma*100).round(0).to_i.to_s.rjust(10, "0")}00000000000000000000000000000000000000000000000000000000000000000000000000000000111111111110000000000000000000000000000000000000000000000000000000000\r\n")
     end
 
     send_file(
