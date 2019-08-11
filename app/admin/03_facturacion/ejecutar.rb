a5090ecb21923d1593dd110ad704546369498246
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-11 13:54:52 -0300

    Inscr

--- app/admin/03_facturacion/ejecutar.rb
+++ app/admin/03_facturacion/ejecutar.rb
@@ -3,13 +3,39 @@ ActiveAdmin.register_page "Ejecutar" do
   menu priority: 301, label: "Ejecutar", parent: "Facturación"
 
 
-  page_action :recargos, method: :post do   
+  page_action :recargos, method: :post do
+
   end
 
-  page_action :facturar, method: :post do   
+  page_action :facturar, method: :post do
+
   end
 
   page_action :sistarbanc, method: :post do   
+    
+    fecha_facturacion = Config.find(1).fecha_facturacion
+    fecha_vencimiento = fecha_facturacion + 9.days
+    mes = fecha_vencimiento.month
+    anio = fecha_vencimiento.year
+
+    file_name = "sistarbanc.txt"
+    file = Tempfile.new(file_name)    
+    File.open(file, "w+") do |f|
+
+      f.write("Año;Mes;Secuencial;Referencia;Nombre;Moneda;Importe;Fecha Vto.;Fecha Inicio;\r\n")
+      Factura.where("fecha='#{fecha_facturacion}'").each do |x|
+        cuenta = x.cuenta_id
+        nombre = Cuenta.where("id=#{x.cuenta_id}").first.nombre
+        importe = x.total
+
+        f.write("#{anio};#{mes};0;#{cuenta};#{nombre};0;#{importe};#{fecha_vencimiento};#{fecha_facturacion};\r\n")
+      end
+    end
+    send_file(
+      file.path,
+      filename: file_name,
+      type: "application/txt"
+    )
   end
 
   page_action :brou, method: :post do   
@@ -23,7 +49,7 @@ ActiveAdmin.register_page "Ejecutar" do
       fecha_vencimiento = fecha_vencimiento + 2.days
     end
 
-    titulo = "FACTURACION #{fecha_facturacion.strftime("%b").upcase}/#{fecha_facturacion.strftime("%Y")}"
+    #titulo = "FACTURACION #{fecha_facturacion.strftime("%b").upcase}/#{fecha_facturacion.strftime("%Y")}"
     dia = fecha_vencimiento.day
     mes = fecha_vencimiento.month
     anio = fecha_vencimiento.year
