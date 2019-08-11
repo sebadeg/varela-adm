2abb34a6fcc4a86ff8a28feec9bd921a6f678669
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-11 13:48:38 -0300

    Inscr

--- app/admin/03_facturacion/ejecutar.rb
+++ app/admin/03_facturacion/ejecutar.rb
@@ -34,20 +34,18 @@ ActiveAdmin.register_page "Ejecutar" do
     file_name = "brou.txt"
     file = Tempfile.new(file_name)    
     File.open(file, "w+") do |f|
-      f.write("#{fecha}\r\n")
+      Factura.where("cuenta_id IN (SELECT id FROM cuentas WHERE brou) AND fecha='#{fecha_facturacion}'").each do |x|
+        cuenta = x.cuenta_id
+        nombre = Cuenta.where("id=#{x.cuenta_id}").first.nombre
+        importe = x.total
+        factura = x.id
 
-       Factura.where("cuenta_id IN (SELECT id FROM cuentas WHERE brou) AND fecha='#{fecha_facturacion}'").each do |x|
-         cuenta = x.cuenta_id
-         nombre = Cuenta.where("id=#{x.cuenta_id}").first.nombre
-         importe = x.total
-         factura = x.id
-
-         str = ("#{nombre} -DEB.AUT.BROU" + ' ' * 48)[0,48]
-         suma = suma + importe 
-         cantidad = cantidad+1
-         f.write("1 00100#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}#{dia.to_s.rjust(2, "0")}#{cuenta}000000000000450798A00000000000#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}00000#{(importe*100).round(0).to_i.to_s.rjust(10, "0")}0000000000000#{str}0000000000000000000000000000000000000000000000000000000000\r\n" )
-       end
-       f.write("2 00100#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}#{dia.to_s.rjust(2, "0")}0000#{cantidad.to_s.rjust(2, "0")}00000000#{(suma*100).round(0).to_i.to_s.rjust(10, "0")}00000000000000000000000000000000000000000000000000000000000000000000000000000000111111111110000000000000000000000000000000000000000000000000000000000\r\n")
+        str = ("#{nombre} -DEB.AUT.BROU" + ' ' * 48)[0,48]
+        suma = suma + importe 
+        cantidad = cantidad+1
+        f.write("1 00100#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}#{dia.to_s.rjust(2, "0")}#{cuenta}000000000000450798A00000000000#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}00000#{(importe*100).round(0).to_i.to_s.rjust(10, "0")}0000000000000#{str}0000000000000000000000000000000000000000000000000000000000\r\n" )
+      end
+      f.write("2 00100#{(anio%100).to_s.rjust(2, "0")}#{mes.to_s.rjust(2, "0")}#{dia.to_s.rjust(2, "0")}0000#{cantidad.to_s.rjust(2, "0")}00000000#{(suma*100).round(0).to_i.to_s.rjust(10, "0")}00000000000000000000000000000000000000000000000000000000000000000000000000000000111111111110000000000000000000000000000000000000000000000000000000000\r\n")
     end
 
     send_file(
