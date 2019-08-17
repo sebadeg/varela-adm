209b490a9753f5ddbc0c702c0782746b2b0006f3
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-17 11:23:41 -0300

    Rubro

--- app/admin/02_cuenta_corriente/movimientos.rb
+++ app/admin/02_cuenta_corriente/movimientos.rb
@@ -34,21 +34,25 @@ ActiveAdmin.register Movimiento do
 
   collection_action :exportar_saico do
 
-    # cantidad = 1
-    # book = Spreadsheet::Workbook.new
-    # sheet = book.create_worksheet
-    # sheet.row(0).push "FechaMov","TipoAsiento","NroCuenta","Detalle","TipoImputacion","ImporteMn","Cotizacion","ImporteMe","Concepto1","Referencia1","Fecha1","Cantidad1","Concepto2","Referencia2","Fecha2","Cantidad2","Concepto3","Referencia3","Fecha3","Cantidad3","Moneda","NroTrf"
-    # Movimiento.where("debe-haber>=0 AND fecha>='2019-01-01' AND rubro_haber!=0").order(:fecha,:cuenta_id).each do |mov|
-    #   sheet.row(cantidad).push ...
-    #   cantidad = cantidad+1
-    # end
-    # Movimiento.where("debe-haber<0 AND fecha>='2019-01-01' AND rubro_haber!=0").order(:fecha,:cuenta_id).each do |mov|
-    #   sheet.row(cantidad).push ...
-    #   cantidad = cantidad+1
-    # end
-    # file_name = "asientos.xls"
-    # file = Tempfile.new(file_name)
-    # book.write file
+    cantidad = 1
+    book = Spreadsheet::Workbook.new
+    sheet = book.create_worksheet
+    sheet.row(0).push "FechaMov","TipoAsiento","NroCuenta","Detalle","TipoImputacion","ImporteMn","Cotizacion","ImporteMe","Concepto1","Referencia1","Fecha1","Cantidad1","Concepto2","Referencia2","Fecha2","Cantidad2","Concepto3","Referencia3","Fecha3","Cantidad3","Moneda","NroTrf"
+    Movimiento.where("debe-haber>=0 AND fecha>='2019-01-01' AND rubro_haber!=0").order(:fecha,:cuenta_id).each do |mov|
+      sheet.row(cantidad).push "#{I18n.l(fecha, format: '%d/%m/%Y')}","VE","#{mov.cuenta_id}","D","#{mov.debe-mov.haber}"
+      cantidad = cantidad+1
+      sheet.row(cantidad).push "#{I18n.l(fecha, format: '%d/%m/%Y')}","VE","#{mov.rubro_id}","C","#{mov.debe-mov.haber}"
+      cantidad = cantidad+1
+    end
+    Movimiento.where("debe-haber<0 AND fecha>='2019-01-01' AND rubro_haber!=0").order(:fecha,:cuenta_id).each do |mov|
+      sheet.row(cantidad).push "#{I18n.l(fecha, format: '%d/%m/%Y')}","VE","#{mov.rubro_id}","D","#{mov.haber-mov.debe}"
+      cantidad = cantidad+1
+      sheet.row(cantidad).push "#{I18n.l(fecha, format: '%d/%m/%Y')}","VE","#{mov.cuenta_id}","C","#{mov.haber-mov.debe}"
+      cantidad = cantidad+1
+    end
+    file_name = "asientos.xls"
+    file = Tempfile.new(file_name)
+    book.write file
 
 
     # file_name = "movimientos.csv"
