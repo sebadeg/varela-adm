b4ca5d5f06be94cf88d617aecd2d97fbf070281b
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-17 14:41:17 -0300

    Rubro

--- app/admin/09_soporte/sector_alumnos.rb
+++ app/admin/09_soporte/sector_alumnos.rb
@@ -1,26 +1,25 @@
 ActiveAdmin.register SectorAlumno do
-
-  permit_params :id, :sectro_id, :alumno_id
+  permit_params :id, :sector_id, :alumno_id
 
   menu priority: 906, label: "Alumnos por Sector", parent: "Soporte"
 
   index do
   	#selectable_column
-    column :sector_id
+    column "Sector" do |c| (c.sector != nil ? "#{c.sector.nombre}" : "" ) end
     column "Alumno" do |c| (c.alumno != nil ? "#{c.alumno.nombre} #{c.alumno.apellido}" : "" ) end
     actions
   end
 
   show do
     attributes_table do
-      row :sector_id
+      row "Sector" do |c| (c.sector != nil ? "#{c.sector.nombre}" : "" ) end
       row "Alumno" do |c| (c.alumno != nil ? "#{c.alumno.nombre} #{c.alumno.apellido}" : "" ) end
     end
   end
 
   form do |f| 
     f.inputs "Alumnos por Sector" do
-      f.input :sector_id
+      f.input :sector, :as => :select, :collection => Sector.all.order(:nombre).map{|u| ["#{u.nombre}",u.id]}
       f.input :alumno, :as => :select, :collection => Alumno.all.order(:nombre,:apellido).map{|u| ["#{u.nombre} #{u.apellido}",u.id]}
     end
     f.actions
