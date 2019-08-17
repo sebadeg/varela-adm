97400bd4e1cf57e974423431cc00a0f34c8921b3
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-17 14:53:50 -0300

    Rubro

--- app/admin/09_soporte/sector_alumnos.rb
+++ app/admin/09_soporte/sector_alumnos.rb
@@ -6,21 +6,25 @@ ActiveAdmin.register SectorAlumno do
   index do
   	#selectable_column
     column "Sector" do |c| (c.sector != nil ? "#{c.sector.nombre}" : "" ) end
-    column "Alumno" do |c| (c.alumno != nil ? "#{c.alumno.nombre} #{c.alumno.apellido}" : "" ) end
+    column "Alumno" do |c| (c.alumno != nil ? "#{u.id} - #{c.alumno.nombre} #{c.alumno.apellido}" : "" ) end
     actions
   end
 
   show do
     attributes_table do
       row "Sector" do |c| (c.sector != nil ? "#{c.sector.nombre}" : "" ) end
-      row "Alumno" do |c| (c.alumno != nil ? "#{c.alumno.nombre} #{c.alumno.apellido}" : "" ) end
+      row "Alumno" do |c| (c.alumno != nil ? "#{u.id} - #{c.alumno.nombre} #{c.alumno.apellido}" : "" ) end
     end
   end
+
+  filter :sector_id, :label => 'Sector', :as => :select, :collection => Sector.all.order(:nombre).map{|u| ["#{u.nombre}",u.id]}
+  filter :alumno_id, :label => 'Alumno', :as => :select, :collection => Alumno.all.order(:nombre,:apellido).map{|u| ["#{u.id} - #{u.nombre} #{u.apellido}", u.id]}
+
 
   form do |f| 
     f.inputs "Alumnos por Sector" do
       f.input :sector, :as => :select, :collection => Sector.all.order(:nombre).map{|u| ["#{u.nombre}",u.id]}
-      f.input :alumno, :as => :select, :collection => Alumno.all.order(:nombre,:apellido).map{|u| ["#{u.nombre} #{u.apellido}",u.id]}
+      f.input :alumno, :as => :select, :collection => Alumno.all.order(:nombre,:apellido).map{|u| ["#{u.id} - #{u.nombre} #{u.apellido}", u.id]}
     end
     f.actions
   end
