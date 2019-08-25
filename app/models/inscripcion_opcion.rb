14b623d0c5541dde320c20b530d8021afca9b52e
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-25 20:05:17 -0300

    InscripcionOpcionCuota

--- app/models/inscripcion_opcion.rb
+++ app/models/inscripcion_opcion.rb
@@ -1,6 +1,9 @@
 class InscripcionOpcion < ApplicationRecord
   belongs_to :inscripcion_opcion_tipo
 
+  has_many :inscripcion_opcion_cuota, :dependent => :delete_all
+  accepts_nested_attributes_for :inscripcion_opcion_cuota, allow_destroy: true
+
 
   def nombre_completo
     s = ""
