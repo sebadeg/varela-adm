1f59a20d56862bf168f573750ded7e8ff0df20a9
Sebastian Degrandi <sebastian.degrandi@ebizmarts.com>
2020-02-01 13:34:11 -0300

    ultimo 2

--- app/admin/03_facturacion/ejecutar.rb
+++ app/admin/03_facturacion/ejecutar.rb
@@ -172,9 +172,9 @@ ActiveAdmin.register_page "Ejecutar" do
 
     #   f.write("\r\n" )
 
-      Inscripcion.where("anio=2020 AND alumno_id IN (111902,114191,115081,121011,130131,130761,138881,139541,139542,139741,140231)").order(:proximo_grado_id,:convenio_id).each do |x|
+      Inscripcion.where("anio=2020 AND ((NOT reinscripcion) OR (reinscripcion AND inscripto)) AND cuotas_id IN (260,269,234,217,88,33,32);").order(:proximo_grado_id,:convenio_id).each do |x|
 
-         f.write("#{x.cuenta_id};#{x.alumno_id};#{x.CalcularPrecioToStr()};#{ProximoGrado.find(x.proximo_grado_id).precio}\r\n" )
+         f.write("#{x.cuenta_id};#{x.alumno_id};#{x.cuotas_id},#{x.CalcularPrecioToStr()};#{ProximoGrado.find(x.proximo_grado_id).precio}\r\n" )
 
        end
 
