b0e1f6f1acb964d4f34599dc117cbedfcbd7a0b2
Sebastian Degrandi <sdegrandi@practia.global>
2020-09-07 17:38:02 -0300

    insc

--- app/models/proximo_grado.rb
+++ app/models/proximo_grado.rb
@@ -4,12 +4,11 @@ class ProximoGrado < ApplicationRecord
   has_many :matricula2020_proximo_grado
   accepts_nested_attributes_for :matricula2020_proximo_grado, allow_destroy: true
 
-
   belongs_to :grado
 
   scope :todos, -> { all }
   scope :corrientes, -> { where("anio IN (SELECT anio_inscripciones FROM configs WHERE NOT anio_inscripciones IS NULL)") }
- 
+
 
   def toString()
   	return "#{nombre} - $U #{precio}"
