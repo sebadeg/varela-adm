54420cc67e071564a4cd3e8ecc3334deb32e3581
Sebastian Degrandi <sdegrandi@practia.global>
2020-09-07 17:42:37 -0300

    insc

--- app/models/matricula2020.rb
+++ app/models/matricula2020.rb
@@ -6,6 +6,9 @@ class Matricula2020 < ApplicationRecord
   has_many :linea_matricula2020, :dependent => :delete_all
   accepts_nested_attributes_for :linea_matricula2020, allow_destroy: true
 
+  has_many :matricula2020_proximo_grado, :dependent => :delete_all
+  accepts_nested_attributes_for :matricula2020_proximo_grado, allow_destroy: true
+
   def toString()
     return "#{nombre}";
   end
