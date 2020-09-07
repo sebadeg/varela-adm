db8c31bf04a3e6963f0a9d79813889f1bbe24001
Sebastian Degrandi <sdegrandi@practia.global>
2020-09-07 09:59:02 -0300

    insc

--- app/models/cuota2020.rb
+++ app/models/cuota2020.rb
@@ -1,4 +1,6 @@
 class Cuota2020 < ApplicationRecord
+  has_many :linea_cuota2020, :dependent => :delete_all
+  accepts_nested_attributes_for :linea_cuota2020, allow_destroy: true
 
   def toString()
     return "#{nombre}";
