6f7d84b1176886955b41439076e576cf8f1f6933
Sebastian Degrandi <sdegrandi@practia.global>
2020-09-11 20:19:14 -0300

    insc

--- app/models/Common.rb
+++ app/models/Common.rb
@@ -1,7 +1,7 @@
 class Common
 
   def self.decimal_to_str(value,places)
-	return (sprintf( "%0.0#{places}}f", descuento).sub(/\.?0+$/, '')
+	return sprintf("%0.0#{places}}f", value).sub(/\.?0+$/, '')
   end
 
 end
