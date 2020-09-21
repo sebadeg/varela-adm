d6aca6d57f45bcbcf2500ec3f6051ed1eca01609
sebadeg <sebastian.degrandi@gmail.com>
2020-09-21 09:21:15 -0300

    Update convenio2020.rb

--- app/models/convenio2020.rb
+++ app/models/convenio2020.rb
@@ -1,7 +1,11 @@
 class Convenio2020 < ApplicationRecord
 
   def toString()
-    return "#{nombre} #{Common.decimal_to_string(descuento,2)}%";
+    if descuento != nil && descuento>0
+      return "#{nombre} #{Common.decimal_to_string(descuento,2)}%";
+    else
+      return ""
+    end
   end
 
 end
